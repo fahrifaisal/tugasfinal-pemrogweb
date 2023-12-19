50ee62c168147961fd6d7f60b0f90a90d636bc41
fahrifaisal <fahrifaisal567@gmail.com>
2023-12-19 20:03:24 +0800

    initiate

---
+++ application/views/_partials/footer.php
@@ -0,0 +1,3 @@
+<footer class="footer">
+	&copy; <?= Date('Y') ?> Tugas Final D121211070-Fahri_Faisal
+</footer>
---
+++ application/views/about.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php $this->load->view('_partials/head.php'); ?>
+</head>
+
+<body>
+	<?php $this->load->view('_partials/navbar.php'); ?>
+	
+	<h1>About Page</h1>
+	<p>Ini adalah tugas final dari Fahri Faisal dengan NIM D121211070<p>
+
+	<?php $this->load->view('_partials/footer.php'); ?>
+</body>
+
+</html>
